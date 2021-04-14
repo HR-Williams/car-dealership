554027cb20aee6253dc6702c29db7e915e68b1a9
HR Williams <halrubinwilliams@gmail.com>
2021-04-14 14:56:56 -0700

    Add missing ford car instance and display all cars in the console

--- Car.cs
+++ Car.cs
@@ -11,7 +11,7 @@ public class Program
   public static void Main()
   {  
     Car volkswagon = new Car();
-    volkswagon.MakeModel = "1974 Volkswagen Thing";
+    volkswagon.MakeModel = "1974 Volkswagon Thing";
     volkswagon.Price = 1100;
     volkswagon.Miles = 368792;
 
@@ -20,9 +20,20 @@ public class Program
     yugo.Price = 700;
     yugo.Miles = 239001;
 
+    Car ford = new Car();
+    ford.MakeModel = "1988 Ford Country Squire";
+    ford.Price = 1400;
+    ford.Miles = 239001;
+
     Car amc = new Car();
     amc.MakeModel = "1976 AMC Pacer";
     amc.Price = 400;
     amc.Miles = 198000;
+
+    List<Car> Cars = new List<Car>() { volkswagon, yugo, ford, amc };
+    foreach (Car automobile in Cars)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
   }
 }
