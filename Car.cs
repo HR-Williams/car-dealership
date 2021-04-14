bb9cb40ad3220aed58c5f64a7b88fe013c117d92
HR Williams <halrubinwilliams@gmail.com>
2021-04-14 14:35:33 -0700

    Create four Car objects in Cars List

--- Car.cs
+++ Car.cs
@@ -10,5 +10,19 @@ public class Program
 {
   public static void Main()
   {  
+    Car volkswagon = new Car();
+    volkswagon.MakeModel = "1974 Volkswagen Thing";
+    volkswagon.Price = 1100;
+    volkswagon.Miles = 368792;
+
+    Car yugo = new Car();
+    yugo.MakeModel = "1980 Yugo Koral";
+    yugo.Price = 700;
+    yugo.Miles = 239001;
+
+    Car amc = new Car();
+    amc.MakeModel = "1976 AMC Pacer";
+    amc.Price = 400;
+    amc.Miles = 198000;
   }
 }
