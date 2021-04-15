49946d0ac073dea4905d996d78b16c6844b6cff8
HR Williams <halrubinwilliams@gmail.com>
2021-04-14 19:52:12 -0700

    Use getter methods to display more details about vehicles

--- Program.cs
+++ Program.cs
@@ -2,8 +2,7 @@ using System;
 using System.Collections.Generic;
 using Dealership.Models;
 
-namespace Dealership
-{
+namespace Dealership {
   public class Program
   {
     public static void Main()
@@ -32,7 +31,10 @@ namespace Dealership
 
       foreach(Car automobile in CarsMatchingSearch)
       {
+        Console.WriteLine("----------------------");
         Console.WriteLine(automobile.GetMakeModel());
+        Console.WriteLine(automobile.GetMiles() + " miles");
+        Console.WriteLine("$" + automobile.GetPrice());
       }
     }
   }
